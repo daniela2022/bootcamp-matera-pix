be02f50e3392de030b2cc2455be1afa96fd22dd4
Daniela Pedon <danielapedon3@gmail.com>
2021-09-28 21:07:51 -0300

    Criando usuario e alterando conta corrente

---
+++ src/main/java/matera/bootcamp/pix/domain/model/Usuario.java
@@ -0,0 +1,27 @@
+package matera.bootcamp.pix.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private String sobrenome;
+	
+	@OneToOne(optional = false)
+	private ContaCorrente contaCorrente;
+	
+
+}
